14cf8321f664011fcd3307cc1bcb80dafb71b28a
sebastianpalarczyk <s.palarczyk12@gmail.com>
2022-02-08 18:57:48 +0100

    added components buttons, container, form, header, section, tasks

---
+++ src/Buttons/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+const Buttons = ({tasks}, {hideDoneTasks}) => (
+    tasks.length > 0 && (
+        <div classNameName="buttons">
+          <button className="buttons__button">
+            {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone 
+          </button>
+          <button className="buttons__button" disabled={tasks.every(({ done }) => done)}>
+            Ukończ wszystkie
+          </button>
+        </div>
+    )
+);
+ 
+export default Buttons;
---
+++ src/Tasks/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+const Tasks = (props) => (
+    <ul className="tasks">
+        {props.tasks.map(task =>(
+            <li className={`tasks__item${task.done && props.hideDoneTasks ? " tasks__item--hidden" : ""}`}>
+                <button className="tasks__button tasks__button--toggleDone">{task.done ? "✓" : ""}</button>
+                <span className={`tasks__content${task.done ? " tasks__content--done" : ""}`}>{task.content}</span>
+                <button className="tasks__button tasks__button--remove">🗑</button>
+            </li>
+        ))}
+    </ul>
+);
+
+export default Tasks;
