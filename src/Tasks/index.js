574f4b065d024b440c3c954cdca433d3299b8fd5
sebastianpalarczyk <s.palarczyk12@gmail.com>
2022-02-10 13:51:37 +0100

    added id to list

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -4,13 +4,15 @@ import "./style.css";
 const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
     <ul className="tasks">
         {tasks.map(task => (
-            <li className={`tasks__item${task.done && hideDone ? " tasks__item--hidden" : ""}`}>
+            <li className={`tasks__item${task.done && hideDone ? " tasks__item--hidden" : ""} id=${task.id}`}>
                 <button
                     className="tasks__button tasks__button--toggleDone"
                     onClick={() => toggleTaskDone(task.id)}
                 >
                     {task.done ? "✓" : ""}</button>
-                <span className={`tasks__content${task.done ? " tasks__content--done" : ""}`}>{task.content}</span>
+                <span className={`tasks__content${task.done ? " tasks__content--done" : ""}`}>
+                    {task.content}
+                </span>
                 <button
                     className="tasks__button tasks__button--remove"
                     onClick={() => removeTask(task.id)}
