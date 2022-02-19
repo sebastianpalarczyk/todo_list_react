2a44cd31b9d08df88bf45c7602b8f1a8259fe71c
sebastianpalarczyk <s.palarczyk12@gmail.com>
2022-02-19 19:57:57 +0100

    added style componetns to Form

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./style.css";
+import { FormContainer, Input, Button } from "./styled.js"
 
 const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
@@ -18,15 +18,14 @@ const Form = ({ addNewTask }) => {
     }
 
     return (
-        <form className="form" onSubmit={onFormSubmit}>
-            <input
+        <FormContainer onSubmit={onFormSubmit}>
+            <Input
                 value={newTaskContent}
-                className="form__input"
                 placeholder="Co jest do zrobienia?"
                 onChange={({ target }) => setNewTaskContent(target.value)}
             />
-            <button className="form__button">Dodaj zadanie</button>
-        </form>
+            <Button> Dodaj zadanie </Button>
+        </FormContainer>
     );
 };
 
