b605efcff5a66bef17860f8b6523196f870d83c5
sebastianpalarczyk <s.palarczyk12@gmail.com>
2022-02-27 17:16:04 +0100

    added file queryParameters

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,28 +1,22 @@
 import React from "react";
-import { useLocation, useHistory } from "react-router-dom";
 import { Input } from "../Input/styled";
 import searchQueryParamName from "../searchQueryParamName";
 import { Wrapper } from "./styled";
+import { useQueryParameter, useReplaceQueryParameter } from "../queryParameters";
 
 const Search = () => {
-    const location = useLocation();
-    const history = useHistory();
-    const query = new URLSearchParams(location.search).get(searchQueryParamName);
+    const query = useQueryParameter(searchQueryParamName);
+    const replaceQueryParameter = useReplaceQueryParameter();
 
     const onInputChange = ({ target }) => {
-        const searchParams = new URLSearchParams(location.search);
-
-        if (target.value.trim() === "") {
-            searchParams.delete(searchQueryParamName);
-        } else {
-            searchParams.set(searchQueryParamName, target.value);
-        }
-
-        history.push(`${location.pathname}?${searchParams.toString()}`);
+        replaceQueryParameter({
+            key: searchQueryParamName,
+            value: target.value.trim() !== "" ? target.value : undefined,
+        })
     };
 
     return (
-        
+
         <Wrapper>
             <Input
                 placeholder="Filtruj zadania"
